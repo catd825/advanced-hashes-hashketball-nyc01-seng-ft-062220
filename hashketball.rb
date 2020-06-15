ca1cb1bf6e6009cd87e25142528f8f7e50e06318
Cathy D'Onofrio <catd825@gmail.com>
2020-06-15 16:28:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,13 +189,13 @@ end
 def player_numbers(team_name)
    numbers=[]
     if game_hash[:home][:team_name] == team_name
-      game_hash[:home][:players].each  {|x|
+      game_hash[:home][:players].each  {|x,y|
         numbers << x[:number]
       }
       numbers
     end
      game_hash[:away][:team_name] == team_name
-      game_hash[:away][:players].each do {|x|
+      game_hash[:away][:players].each do {|x,y|
         numbers << x[:number]
       }
      numbers
