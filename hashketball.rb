54f8a493e769b90a66efe15b520e11836ed9e3f0
Cathy D'Onofrio <catd825@gmail.com>
2020-06-15 17:05:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -224,7 +224,7 @@ def big_shoe_rebounds
   game_hash.each do |location, key_hash|
     key_hash.each do |players, detail|
        if players == :players
-        detail.each do |player_name_detail|
+         player_name_detail[:points]
       binding.pry
         end
       end
